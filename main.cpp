995e2ab210247738a6d270a6ff310d3457b3fe6f
Alain CELESTE <alain.celeste@silentsensors.com>
2018-09-21 17:55:00 +0200

    Modified program to toggle led2 every 30 seconds

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 
-DigitalOut led(LED1);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
 
 time_t target_fire_time;
 
@@ -12,19 +13,21 @@ void checkRTCTimeout()
 {
     timeout = false;
     time_t now = time(NULL);
+    led2 = !led2;
     if ( now >= target_fire_time ) timeout = true;
 }
 
 int main()
 {
-    led = 1; // LED is ON
+    led1 = 1; // LED is ON
+    led2 = 0; // LED is ON
     target_fire_time = time(NULL) + 180;
     halfminute.attach(&checkRTCTimeout, 30); /* will tick every 30 seconds */ 
     while(1) {
         while(!timeout) {
             sleep();
         }
-        led = !led; /* toggle led */
+        led1 = !led1; /* toggle led */
         target_fire_time = time(NULL) + 180; /* set timeout to 3 minutes */
         timeout = false;
     }
